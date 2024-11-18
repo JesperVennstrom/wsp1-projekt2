b8c5f756bb5b31b74f02d810f41224c0469c29e3
JesperVennstrom <jesper.vennstrom@elev.ga.ntig.se>
2024-11-18 13:34:57 +0100

    1a ver

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 class App < Sinatra::Base
 
+    def db
+        return @db if @db
+  
+        @db = SQLite3::Database.new("db/todos.sqlite")
+        @db.results_as_hash = true
+  
+        return @db
+    end
     get '/' do
+        @todos = db.execute('SELECT * FROM todos')
         erb(:"index")
     end
 
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,7 +3,33 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
+  end
+
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todos')
+  end
+  def self.create_tables
+    db.execute('CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                description TEXT,
+                completed BOOLEAN DEFAULT FALSE)')
+  end
+  def self.populate_tables
+    db.execute('INSERT INTO todos (title, description, completed) VALUES ("Gymma", "Gå till gymmet och kör ett bröstpass", 1)')
+    db.execute('INSERT INTO todos (title, description) VALUES ("Laga mat", "Stek fläskytterfile och grädda potatisgratäng")')
+    db.execute('INSERT INTO todos (title, description) VALUES ("Städa", "Dammsuga och torka golven")')
+    db.execute('INSERT INTO todos (title, description) VALUES ("Läsa", "Läs ut boken om Steve Jobs")')
+  end
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todos.sqlite')
+    @db.results_as_hash = true
+    @db
   end
 
 end
