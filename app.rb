87c6be91c3b906338df463631e70f95fe2adab62
JesperVennstrom <jesper.vennstrom@elev.ga.ntig.se>
2024-11-25 14:23:01 +0100

    V2
    Glömde förra lektionen att commita

--- app.rb
+++ app.rb
@@ -9,8 +9,30 @@ class App < Sinatra::Base
         return @db
     end
     get '/' do
+        redirect('/login')
+    end
+    get '/login' do
+        erb(:login)
+    end
+    get '/todos' do
         @todos = db.execute('SELECT * FROM todos')
-        erb(:"index")
+        erb(:index)
+    end
+    post '/todos' do
+        db.execute('INSERT INTO todos (title, description) VALUES (?, ?)', [params[:title], params[:description]])
+        redirect('/')
+    end
+    post '/todos/:id/delete' do
+        db.execute('DELETE FROM todos WHERE id = ?', [params[:id]])
+        redirect('/')
+    end
+    post '/todos/:id/:completed' do |id, completed|
+        if completed == '0' 
+            db.execute('UPDATE todos SET completed = 1 WHERE id = ?', [params[:id]])
+        elsif completed == '1'
+            db.execute('UPDATE todos SET completed = 0 WHERE id = ?', [params[:id]])
+        end
+        redirect('/')
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -17,12 +17,18 @@ class Seeder
                 title TEXT NOT NULL,
                 description TEXT,
                 completed BOOLEAN DEFAULT FALSE)')
+    db.execute('CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL)')
   end
   def self.populate_tables
     db.execute('INSERT INTO todos (title, description, completed) VALUES ("Gymma", "Gå till gymmet och kör ett bröstpass", 1)')
     db.execute('INSERT INTO todos (title, description) VALUES ("Laga mat", "Stek fläskytterfile och grädda potatisgratäng")')
     db.execute('INSERT INTO todos (title, description) VALUES ("Städa", "Dammsuga och torka golven")')
     db.execute('INSERT INTO todos (title, description) VALUES ("Läsa", "Läs ut boken om Steve Jobs")')
+
+    db.execute('INSERT INTO users (username, password) VALUES ("admin", "admin")')
   end
   private
   def self.db
