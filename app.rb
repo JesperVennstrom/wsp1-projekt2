68c48e9feccab18b840aa2e8794f7fdae48474a2
JesperVennstrom <jesper.vennstrom@elev.ga.ntig.se>
2024-12-09 13:12:21 +0100

    Login + Register
    Login + Register

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
+require 'sinatra'
+require 'securerandom'
+
 class App < Sinatra::Base
+    
+    configure do
+        enable :sessions
+        set :session_secret, SecureRandom.hex(64)
+    end
 
     def db
         return @db if @db
@@ -12,19 +20,35 @@ class App < Sinatra::Base
         redirect('/login')
     end
     get '/login' do
-        erb(:login)
+        if !session[:user]
+            erb(:login)
+        else
+            redirect('/todos')
+        end
+    end
+    post '/login' do
+        user = db.execute('SELECT * FROM users WHERE username = ?', [params[:username]]).first
+
+        if user && user['password'] == params[:password]
+            session[:user] = user
+        else 
+            status 401
+            @error = 'Fel användarnamn eller lösenord'
+        end
+        redirect('/login')
     end
     get '/todos' do
-        @todos = db.execute('SELECT * FROM todos')
+        @todos = db.execute('SELECT * FROM todos WHERE user_id = ?', [session[:user]['id']])
+
         erb(:index)
     end
     post '/todos' do
-        db.execute('INSERT INTO todos (title, description) VALUES (?, ?)', [params[:title], params[:description]])
-        redirect('/')
+        db.execute('INSERT INTO todos (title, description, user_id) VALUES (?, ?, ?)', [params[:title], params[:description], session[:user]['id']])
+        redirect('/todos')
     end
     post '/todos/:id/delete' do
         db.execute('DELETE FROM todos WHERE id = ?', [params[:id]])
-        redirect('/')
+        redirect('/todos')
     end
     post '/todos/:id/:completed' do |id, completed|
         if completed == '0' 
@@ -32,7 +56,19 @@ class App < Sinatra::Base
         elsif completed == '1'
             db.execute('UPDATE todos SET completed = 0 WHERE id = ?', [params[:id]])
         end
-        redirect('/')
+        redirect('/todos')
+    end
+
+    post '/logout' do
+        session.clear
+        redirect('/login')
+    end
+    get '/register' do
+        erb(:register)
+    end
+    post '/register' do
+        db.execute('INSERT INTO users (username, password) VALUES (?, ?)', [params[:username], params[:password]])
+        redirect('/login')
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -10,25 +10,28 @@ class Seeder
 
   def self.drop_tables
     db.execute('DROP TABLE IF EXISTS todos')
+    db.execute('DROP TABLE IF EXISTS users')
   end
   def self.create_tables
-    db.execute('CREATE TABLE todos (
+    db.execute('CREATE TABLE todos(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT NOT NULL,
                 description TEXT,
-                completed BOOLEAN DEFAULT FALSE)')
+                completed BOOLEAN DEFAULT FALSE,
+                user_id INTEGER NOT NULL)')
     db.execute('CREATE TABLE users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL,
                 password TEXT NOT NULL)')
   end
   def self.populate_tables
-    db.execute('INSERT INTO todos (title, description, completed) VALUES ("Gymma", "Gå till gymmet och kör ett bröstpass", 1)')
-    db.execute('INSERT INTO todos (title, description) VALUES ("Laga mat", "Stek fläskytterfile och grädda potatisgratäng")')
-    db.execute('INSERT INTO todos (title, description) VALUES ("Städa", "Dammsuga och torka golven")')
-    db.execute('INSERT INTO todos (title, description) VALUES ("Läsa", "Läs ut boken om Steve Jobs")')
+    db.execute('INSERT INTO todos (title, description, completed, user_id) VALUES ("Gymma", "Gå till gymmet och kör ett bröstpass", 1, 1)')
+    db.execute('INSERT INTO todos (title, description, user_id) VALUES ("Laga mat", "Stek fläskytterfile och grädda potatisgratäng", 1)')
+    db.execute('INSERT INTO todos (title, description, user_id) VALUES ("Städa", "Dammsuga och torka golven", 1)')
+    db.execute('INSERT INTO todos (title, description, user_id) VALUES ("Läsa", "Läs ut boken om Steve Jobs", 1)')
 
     db.execute('INSERT INTO users (username, password) VALUES ("admin", "admin")')
+    
   end
   private
   def self.db
